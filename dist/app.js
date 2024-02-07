4051d5827f1319f9c49f5b59ba2b72599a1310af
Samuel Brewer <sam.brewer13@yahoo.com>
2024-02-07 12:47:27 -0700

    rest operator

--- dist/app.js
+++ dist/app.js
@@ -1,17 +1,16 @@
 "use strict";
-const userName = 'Sam';
-let age = 26;
-age = 27;
-const add = (a, b = 1) => a + b;
-if (age > 20) {
-    let isOld = true;
-}
-const printOutput = (output) => console.log(output);
-const button = document.querySelector('button');
-if (button) {
-    button.addEventListener('click', (event) => console.log(event));
-}
-printOutput(add(5));
 const hobbies = ['Sports', 'Cooking'];
 const activeHobbies = ['Hiking', ...hobbies];
 activeHobbies.push(...hobbies);
+const person = {
+    name: 'Sam',
+    age: 32,
+};
+const copiedPerson = Object.assign({}, person);
+function add(...numbers) {
+    return numbers.reduce((currentResult, currentValue) => {
+        return currentResult + currentValue;
+    }, 0);
+}
+const addedNumbers = add(5, 10, 15, 20, 25, 30);
+console.log(addedNumbers);
