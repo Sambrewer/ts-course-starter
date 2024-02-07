35b821ad326e4ab5b737d44a5505a828ece002ca
Samuel Brewer <sam.brewer13@yahoo.com>
2024-02-07 12:29:48 -0700

    arrow functions

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
-"use strict";
 const userName = 'Sam';
+// userName = "Samuel";
 let age = 26;
+
 age = 27;
-const add = (a, b) => {
+
+const add = (a: number, b: number) => {
     let result;
     result = a + b;
     return result;
 };
+
 if (age > 20) {
     let isOld = true;
 }
+
+// console.log(isOld);
